1417e5e8906c6d8fcc7916dc0dde1c31b15067ff
Lukas Jonathan Haeuser <lhaeuser@ethz.ch>
2025-10-15 11:03:12 +0200

    feat: added lazy imports

--- singlecellrnasignature/__init__.py
+++ singlecellrnasignature/__init__.py
@@ -1,2 +1,11 @@
-import singlecellrnasignature.raw
-import singlecellrnasignature.adata
+from importlib import import_module
+from typing import Any
+
+__all__ = ["adata", "raw"]
+
+def __getattr__(name: str) -> Any:
+    if name in __all__:
+        mod = import_module(__name__ + "." + name)
+        globals()[name] = mod
+        return mod
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
